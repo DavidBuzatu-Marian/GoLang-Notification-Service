8e31b0cef63ee91e4a76b48eea815111fe95f050
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-07-20 08:10:13 +0100

    Added config read

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	MongoURI string
+}
+
+func readConfig() {
+	file, _ := os.Open("./config/default.json")
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	config := Config{}
+	err := decoder.Decode(&config)
+	if err != nil {
+		fmt.Println("Error reading config file!")
+		os.Exit(1)
+	}
+}
 
 func main() {
+	readConfig()
 	fmt.Println("Hello from GoLang!")
 }
