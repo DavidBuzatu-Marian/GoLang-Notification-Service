df5641e3d6118ac13850ff62eb83e6cdb59356ff
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-07-27 22:13:49 +0100

    Fixed issue with connect

--- scheduler.go
+++ scheduler.go
@@ -10,6 +10,7 @@ import (
 func Schedule(repeatInterval time.Duration) {
 	for {
 		// SendNotification("Test", "Test message from GoLang")
+		go_mongo.ConnectToMongo()
 		people := go_mongo.CollectBirthdays()
 		for _, val := range people {
 			fmt.Println(val)
