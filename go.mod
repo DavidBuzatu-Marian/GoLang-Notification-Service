f415b3ccce4c925ba355d75c2a06134ebc75299e
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-07-28 20:42:15 +0100

    Added message for notification

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/DavidBuzatu-Marian/go_mongo v0.0.0-20210728191952-3e247305c28d
 	github.com/gen2brain/beeep v0.0.0-20210529141713-5586760f0cc1
+	go.mongodb.org/mongo-driver v1.7.0
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 )
--- scheduler.go
+++ scheduler.go
@@ -2,19 +2,31 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/DavidBuzatu-Marian/go_mongo"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func Schedule(repeatInterval time.Duration) {
 	for {
-		// SendNotification("Test", "Test message from GoLang")
 		client := go_mongo.ConnectToMongo(config.MongoURI)
 		people := go_mongo.CollectBirthdays(client)
 		for _, val := range people {
 			fmt.Println(val)
 		}
+		message := createNotificationMessageUsingPeopleInfo(people)
+		SendNotification("Birthdays today", message)
 		<-time.After(repeatInterval * time.Second)
 	}
+}
+
+func createNotificationMessageUsingPeopleInfo(people []bson.D) string {
+	var message strings.Builder
+	for _, person := range people {
+		personMap := person.Map()
+		fmt.Fprintf(&message, "Name: %s; Email: %s; PhoneNumber: %s", personMap["name"], personMap["email"], personMap["phoneNumber"])
+	}
+	return message.String()
 }
